f7f92aaffb851d811dc54c6e2deebcf18db8db70
RajNalla123 <rajashekarreddy606@gmail.com>
2022-10-19 17:11:24 +0530

    push the code

---
+++ src/test/java/Rajhithub/Rajgitclass.java
@@ -0,0 +1,18 @@
+package Rajhithub;
+
+public class Rajgitclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=12;
+		int b=45;
+		int c=34;
+		
+		System.out.println("my name is raj");
+
+	}
+
+
+	}
+
+}
