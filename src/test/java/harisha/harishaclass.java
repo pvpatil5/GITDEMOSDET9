836025165f8f980447e7d6576936d677d0b04cdc
Harishanaga <nagaharisha99@gmail.com>
2022-10-19 17:21:24 +0530

    Merge branch 'master' of https://github.com/pvpatil5/GITDEMOSDET9.git

--- src/test/java/harisha/harishaclass.java
+++ src/test/java/harisha/harishaclass.java
@@ -5,6 +5,7 @@ public class harishaclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("harisha  ");
+		System.out.println("TYSS HYD");
 	}
 
 }
