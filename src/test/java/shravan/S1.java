66451c0bab6ecc5d0b441fe644e54fc1e6052cae
chillale <chillalesravankumar1995@gmail.com>
2022-10-20 15:42:19 +0530

    make changes in shravan

--- src/test/java/shravan/S1.java
+++ src/test/java/shravan/S1.java
@@ -3,7 +3,8 @@ package shravan;
 public class S1 {
 
 	public static void main(String[] args) {
-		System.out.println("s1");
+		System.out.println("sravan is a good boy");
+		System.out.println("venkat is also a good boy");
 
 	}
 
