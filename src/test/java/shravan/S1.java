5bde1c0e5e9e5b2a0808238533a8f0553d0de92c
chillale <chillalesravankumar1995@gmail.com>
2022-10-20 15:47:53 +0530

    changes has done

--- src/test/java/shravan/S1.java
+++ src/test/java/shravan/S1.java
@@ -5,6 +5,7 @@ public class S1 {
 	public static void main(String[] args) {
 		System.out.println("sravan is a good boy");
 		System.out.println("venkat is also a good boy");
+		System.out.println("raju is a good boygit");
 
 	}
 
